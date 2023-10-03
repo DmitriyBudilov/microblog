ea4f964856776285bd39ac777133c096a3c8adba
Dmitry <budilovdmi@gmail.com>
2023-10-03 17:23:00 +1200

    d: 031023 c: add errors handlers

--- application/forms.py
+++ application/forms.py
@@ -4,13 +4,15 @@ from wtforms import (
     EmailField,
     PasswordField,
     BooleanField,
+    TextAreaField,
     SubmitField
 )
 from wtforms.validators import (
     DataRequired,
+    Length,
     Email,
-    ValidationError,
-    EqualTo
+    EqualTo,
+    ValidationError
 )
 
 from application.models import User
@@ -75,3 +77,16 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField(
+        label='Username',
+        validators=[DataRequired()]
+    )
+    about_me = TextAreaField(
+        label='About me',
+        validators=[Length(min=0, max=140)]
+    )
+    submit = SubmitField(
+        label='Submit'
+    )
--- application/models.py
+++ application/models.py
@@ -30,6 +30,13 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(
         db.String(128)
     )
+    about_me = db.Column(
+        db.String(140)
+    )
+    last_seen = db.Column(
+        db.DateTime, default=datetime.utcnow
+    )
+
     posts = db.relationship(
         'Post',
         backref='author',
