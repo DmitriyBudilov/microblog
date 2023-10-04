97bc2ccfc2051932853af234ba4f689ac8332dcc
Dmitry <budilovdmi@gmail.com>
2023-10-04 17:13:03 +1200

    d: 041023 c: add follower

--- application/__init__.py
+++ application/__init__.py
@@ -1,16 +1,51 @@
+import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
 from flask import Flask
-from flask_login import LoginManager
 
 from .models import db, migrate, login
 
 app = Flask(
     import_name=__name__,
-    # instance_relative_config=False
+    instance_relative_config=False
     )
 app.config.from_object("config.DevelopmentConfig")
 
 login.init_app(app)
 db.init_app(app)
 migrate.init_app(app, db)
+
+if not app.debug:
+    # Mail
+    # if app.config['MAIL_SERVER']:
+    #     auth = None
+    #     if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+    #         auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    #     secure = ()
+    #     if app.config['MAIL_USE_TLS']:
+    #         secure = ()
+    #     mail_handler = SMTPHandler(
+    #         mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+    #         fromaddr='no-reply@' + app.config['MAIL_SERVEER'],
+    #         toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+    #         credentials=auth, secure=secure
+    #     )
+    #     mail_handler.setLevel(logging.ERROR)
+    #     app.logger.addHandler(mail_handler)
+
+    # To File
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
 
 from application import routes, models, errors
--- application/models.py
+++ application/models.py
@@ -11,6 +11,12 @@ migrate = Migrate()
 login = LoginManager()
 login.login_view = 'login'
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
+)
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(
@@ -43,6 +49,15 @@ class User(UserMixin, db.Model):
         lazy='dynamic'
     )
 
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
+
     def __repr__(self):
         return f'User:\t{self.username}\n'
 
@@ -55,6 +70,24 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+        ).count() > 0
+    
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
     
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -78,6 +111,7 @@ class Post(db.Model):
     def __repr__(self):
         return f'Post: \n\t{self.body}\n'
         
+
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
